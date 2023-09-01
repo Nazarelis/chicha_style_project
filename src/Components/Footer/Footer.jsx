85fed05ac92496b6d07a8b6b25834484fa7cb6cc
nazarelis <anabelojeda2001@gmail.com>
2023-09-01 12:03:18 -0300

    cambios con media queries

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,14 +3,14 @@ import './Footer.css';
 const footer = () => {
     return(
 
-        <footer>
+        <footer> 
             <div className='container-footer'>
                 <div className='icon-footer'>
-                <img src="/assets/icon-footer.png" />
+                <img className='img-footer' src="/assets/icon-footer.png" />
                 </div>
                 <p id='text-footer'>©2023 Todos los derechos Reservados · Diseñado por Di Polvere, Ortiz, Takahashi · Desarrollado por Mazzaccaro Alan, Ojeda Anabel </p>
                 <div className='icon-footer'>
-                <img src="/assets/icon-footer.png" />
+                <img className='img-footer' src="/assets/icon-footer.png" />
                 </div>  
             </div>
         </footer>
